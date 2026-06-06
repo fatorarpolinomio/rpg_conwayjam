92f35e6629ead54c49f8aafc0cebca781bbc5cbf
Paulo Henrique Silva Borges <paulohenriquesb.2007@gmail.com>
2026-06-06 15:47:59 -0300

    feat: adicionado classes e atributos de itens

---
+++ src/interno/itens/itens.hpp
@@ -0,0 +1,33 @@
+#ifndef ITENS_H
+#define ITENS_H
+
+#include <stdlib.h>
+#include <string>
+
+class Item {
+private:
+    std::string nome;
+    std::string tipo;
+    double durabilidade;
+    double dano;
+    double regeneracao;
+    std::string alvo;
+
+public:
+    Item();
+
+    std::string GetTipo() {return tipo;}
+    double GetDurabilidade() {return durabilidade;}
+    double GetDano() {return dano;}
+    double GetRegeneracao() {return regeneracao;}
+    std::string GetAlvo() {return alvo;}
+
+    void SetTipo(std::string newTipo) {tipo = newTipo;}
+    void SetDurabilidade(double newDur) {durabilidade = newDur;}
+    void SetDano(double newDano) {dano = newDano;}
+    void SetRegeneracao(double newReg) {regeneracao = newReg;}
+    void SetAlvo(std::string newAlvo) {alvo = newAlvo;}
+
+    void Use(Item item, Protagonista protagonista, Inimigo inimigo);
+};
+#endif
