1a940e7ebd7d16d54e75d46745ad9db55dacd5c1
fatorarpolinomio <xextreme225@gmail.com>
2026-06-06 18:35:28 -0300

    fix(itens.cpp): conserta barra invertida

--- src/interno/itens/itens.cpp
+++ src/interno/itens/itens.cpp
@@ -1,80 +1,77 @@
 #include <cstdlib>
 #include <string>
 
+#include "../inimigos/inimigos.hpp"
+#include "../protagonista/protagonista.hpp"
 #include "itens.hpp"
-#include "..\protagonista\protagonista.hpp"
-#include "..\inimigos\inimigos.hpp"
 
 Item::Item() {}
 
 Item Xarope() {
-    Item xarope;
-    xarope.SetTipo("Consumível");
-    xarope.SetRegeneracao(20);
-    xarope.SetAlvo("Protagonista");
-    return xarope;
+  Item xarope;
+  xarope.SetTipo("Consumível");
+  xarope.SetRegeneracao(20);
+  xarope.SetAlvo("Protagonista");
+  return xarope;
 }
 
 Item Tanque() {
-    Item tanque;
-    tanque.SetTipo("Consumível");
-    tanque.SetRegeneracao(100);
-    tanque.SetAlvo("Oxigênio");
-    return tanque;
+  Item tanque;
+  tanque.SetTipo("Consumível");
+  tanque.SetRegeneracao(100);
+  tanque.SetAlvo("Oxigênio");
+  return tanque;
 }
 
 Item Capacete() {
-    Item capacete;
-    capacete.SetTipo("Equipável");
-    capacete.SetDurabilidade(100);
-    return capacete;
+  Item capacete;
+  capacete.SetTipo("Equipável");
+  capacete.SetDurabilidade(100);
+  return capacete;
 }
 
 Item SilverTape() {
-    Item silverTape;
-    silverTape.SetTipo("Consumível");
-    silverTape.SetRegeneracao(20);
-    silverTape.SetAlvo("Capacete");
-    return silverTape;
+  Item silverTape;
+  silverTape.SetTipo("Consumível");
+  silverTape.SetRegeneracao(20);
+  silverTape.SetAlvo("Capacete");
+  return silverTape;
 }
 
 Item Martelo() {
-    Item martelo;
-    martelo.SetTipo("Arma");
-    martelo.SetDano(20);
-    return martelo;
+  Item martelo;
+  martelo.SetTipo("Arma");
+  martelo.SetDano(20);
+  return martelo;
 }
 
 Item Chave() {
-    Item chave;
-    chave.SetTipo("Chave");
-    return chave;
+  Item chave;
+  chave.SetTipo("Chave");
+  return chave;
 }
 
 Item Ferramentas() {
-    Item ferramentas;
-    ferramentas.SetTipo("Chave");
-    return ferramentas;
+  Item ferramentas;
+  ferramentas.SetTipo("Chave");
+  return ferramentas;
 }
 
-
-void usarItem(Item item, Protagonista& protagonista, Inimigo& inimigo) {
-    if (item.GetTipo() == "Consumível") {
-        if (item.GetAlvo() == "Protagonista") {
-            protagonista.aumentarIntegridade(item.GetRegeneracao());
-        }
-        else if (item.GetAlvo() == "Capacete") {
-            Item capacete = Capacete();
-            capacete.SetDurabilidade(capacete.GetDurabilidade() + item.GetRegeneracao());
-        }
-        else if (item.GetAlvo() == "Oxigênio") {
-            protagonista.aumentarOxigenio(item.GetRegeneracao());
-        }
+void usarItem(Item item, Protagonista &protagonista, Inimigo &inimigo) {
+  if (item.GetTipo() == "Consumível") {
+    if (item.GetAlvo() == "Protagonista") {
+      protagonista.aumentarIntegridade(item.GetRegeneracao());
+    } else if (item.GetAlvo() == "Capacete") {
+      Item capacete = Capacete();
+      capacete.SetDurabilidade(capacete.GetDurabilidade() +
+                               item.GetRegeneracao());
+    } else if (item.GetAlvo() == "Oxigênio") {
+      protagonista.aumentarOxigenio(item.GetRegeneracao());
     }
-    else if (item.GetTipo() == "Arma") {
-        inimigo.TomarDano(item.GetDano());
-    }
-    else if (item.GetTipo() == "Equipável") {
-        protagonista.setIntegridade(protagonista.getIntegridade() + item.GetDurabilidade());
-    }
+  } else if (item.GetTipo() == "Arma") {
+    inimigo.TomarDano(item.GetDano());
+  } else if (item.GetTipo() == "Equipável") {
+    protagonista.setIntegridade(protagonista.getIntegridade() +
+                                item.GetDurabilidade());
+  }
 }
