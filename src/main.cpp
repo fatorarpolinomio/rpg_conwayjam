90449c49cf1a80fb19c464bd053248dcb78c2749
fatorarpolinomio <xextreme225@gmail.com>
2026-06-06 18:45:48 -0300

    feat(entidades): nova pasta para abrigar inimigos, itens e protagonista

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,34 @@
-#include <cstdlib>
+#include "interno/entidades/protagonista/protagonista.hpp"
 #include "raylib.h"
+#include <cstdlib>
 #include <iostream>
-#include "interno/protagonista/protagonista.hpp"
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 400
 
 int main() {
 
-	
-	InitWindow(WINDOW_WIDTH,WINDOW_HEIGHT,"RPG");
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "RPG");
 
-	InitAudioDevice();  
-	if(!IsAudioDeviceReady()){
-		std::cout << "ERROR: Dispositivo de som não foi inicializado" << endl;
-	}             
+  InitAudioDevice();
+  if (!IsAudioDeviceReady()) {
+    std::cout << "ERROR: Dispositivo de som não foi inicializado" << endl;
+  }
 
-	Protagonista violeta(Vector2{20,20});
+  Protagonista violeta(Vector2{20, 20});
 
-	while (!WindowShouldClose())
-	{
-		
-		violeta.Update();
+  while (!WindowShouldClose()) {
 
-		BeginDrawing();
-			ClearBackground(WHITE);
-			
-			violeta.Draw();
-		EndDrawing();
-	}
-	
-	CloseAudioDevice();               
-	CloseWindow();
-	return 0;
+    violeta.Update();
+
+    BeginDrawing();
+    ClearBackground(WHITE);
+
+    violeta.Draw();
+    EndDrawing();
+  }
+
+  CloseAudioDevice();
+  CloseWindow();
+  return 0;
 }
