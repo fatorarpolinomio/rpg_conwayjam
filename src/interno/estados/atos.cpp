52afacb2dfa470b28f100e96728239a0c2c8a35f
Gustavo <gustavosantos.202507@gmail.com>
2026-06-11 20:07:17 -0300

    fix: arrumei os diretorios pro windows

--- src/interno/estados/atos.cpp
+++ src/interno/estados/atos.cpp
@@ -44,16 +44,15 @@ void Atos::iniciarAto(HistoryState novoAto) {
             new NPC("../../../assets/Spritesheets/NPCS/tripulante7.png",Vector2{800,150})
         };
 
-       	new Item ("Consumível", 50, "Infecção", "../assets/Spritesheets/Itens/Xarope.png", { 600,750 });
-        new Item ("Consumível", 100, "Oxigênio", "../assets/Spritesheets/Itens/TanqueO2.png", { 1000, 1000 });
-        new Item ("Consumível", 30, "Integridade", "../assets/Spritesheets/Itens/SilverTape.png", { 1000,1000 });
-        new Item ("Consumível", 100, "Integridade", "../assets/Spritesheets/Itens/Capacete.png", { 1000,1000 });
-        new Item ("Arma", 20, "", "../assets/Spritesheets/Itens/Martelo.png", { 1000,1000 });
-        new Item ("Arma", 50, "", "../assets/Spritesheets/Itens/Taser.png", { 1000,1000 });
-        new Item ("Chave", 0, "", "../assets/Spritesheets/Itens/KeyCard.png", { 1000,1000 });
-        new Item ("Chave", 0, "", "../assets/Spritesheets/Itens/CaixaDeFerramentas.png", { 1000,1000 });
-
-        new Item ("Consumivel", 0, "Chave", "../assets/Spritesheets/Itens/CaixaDeFerramentas.png", Vector2{200,1150});
+       	new Item ("Consumível", 50, "Infecção", "../../../assets/Spritesheets/Itens/Xarope.png", { 600,750 });
+        new Item ("Consumível", 100, "Oxigênio", "../../../assets/Spritesheets/Itens/TanqueO2.png", { 1000, 1000 });
+        new Item ("Consumível", 30, "Integridade", "../../../assets/Spritesheets/Itens/SilverTape.png", { 1000,1000 });
+        new Item ("Consumível", 100, "Integridade", "../../../assets/Spritesheets/Itens/Capacete.png", { 1000,1000 });
+        new Item ("Arma", 20, "", "../../../assets/Spritesheets/Itens/Martelo.png", { 1000,1000 });
+        new Item ("Arma", 50, "", "../../../assets/Spritesheets/Itens/Taser.png", { 1000,1000 });
+        new Item ("Chave", 0, "", "../../../assets/Spritesheets/Itens/KeyCard.png", { 1000,1000 });
+        new Item ("Chave", 0, "", "../../../assets/Spritesheets/Itens/CaixaDeFerramentas.png", { 1000,1000 });
+        new Item ("Consumivel", 0, "Chave", "../../../assets/Spritesheets/Itens/CaixaDeFerramentas.png", Vector2{200,1150});
 
         // - Dar spawn nos NPCs iniciais
     } else if(novoAto == HistoryState::ACT_1){
